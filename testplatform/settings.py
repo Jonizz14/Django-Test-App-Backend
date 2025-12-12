e2d1d749886e439e7e72e9190adbf09d4189e8ef
Jonizz14 <jahongirtuhtaev989@gmail.com>
2025-12-12 16:59:01 +0500

    sas

--- testplatform/settings.py
+++ testplatform/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3kz$-%49jf(w$+3#$ik*u9(=#id+r+$jggv^!%#_=l1k6ff7j('
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3kz$-%49jf(w$+3#$ik*u9(=#id+r+$jggv^!%#_=l1k6ff7j(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,django-test-app-backend.onrender.com').split(',')
 
 AUTH_USER_MODEL = 'api.User'
 
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (Images, Videos, etc.)
 MEDIA_URL = '/media/'
